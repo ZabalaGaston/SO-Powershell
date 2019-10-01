026665dbae992e2a2a12f812e91c93b8fd8f393a
ZabalaGaston <49344960+ZabalaGaston@users.noreply.github.com>
2019-10-01 11:41:49 -0300

    v1.0
    #Pendiente, respuesta 2

---
+++ ejercicio1/ejercicio1.ps1
@@ -0,0 +1,36 @@
+# Trabajo práctico N2 Ejercicio 1
+# Script: ejercicio1.ps1
+# Integrantes:
+# Cabral, David         39757782
+# Cela, Pablo           36166867
+# Pessolani, Agustin    39670584
+# Sullca, Fernando      37841788
+# Zabala, Gaston        34614948
+
+Param (
+[Parameter(Position = 1, Mandatory = $false)]
+[String] $pathsalida = ".\procesos.txt ",
+[int] $cantidad = 3
+)
+$existe = Test-Path $pathsalida
+if ($existe -eq $true) {
+$listaproceso = Get-Process
+foreach ($proceso in $listaproceso) {
+$proceso | Format-List -Property Id,Name >> $pathsalida
+}
+for ($i = 0; $i -lt $cantidad ; $i++) {
+Write-Host $listaproceso[$i].Name - $listaproceso[$i].Id
+}
+} else {
+Write-Host "El path no existe"
+}
+
+
+#1. ¿Cuál es el objetivo del script?
+# El objetivo del script es generar un archivo con los procesos que están corriendo. Además, muestra el 
+#nombre del proceso y id de tres procesos
+
+#2. ¿Agregaría alguna otra validación a los parámetros?
+
+#3. ¿Qué sucede si se ejecuta el script sin ningún parámetro?
+# El script arroja el error "El path no existe"
